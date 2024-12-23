9184f829cc1d3ddd1cc120515b242b4af0994198
aixion1506 <aixion1506@gmail.com>
2024-12-23 17:58:27 +0900

    Feat: 레코드 검색과 필터링

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 
@@ -9,5 +9,15 @@ export class UsersController {
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
     this.userServcie.create(body.email, body.password);
+  }
+
+  @Get('/:id')
+  findUser(@Param('id') id: string) {
+    return this.userServcie.findOne(parseInt(id));
+  }
+
+  @Get()
+  findAllUser(@Query('email') email: string) {
+    return this.userServcie.find(email);
   }
 }
