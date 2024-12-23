687186e497672df41d369cc405ccc4a13dc005e4
aixion1506 <aixion1506@gmail.com>
2024-12-23 15:04:06 +0900

    Feat: User 생성 및 DB 저장

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
+  constructor(private userServcie: UsersService) {}
+
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
-    console.log(body);
+    this.userServcie.create(body.email, body.password);
   }
 }
