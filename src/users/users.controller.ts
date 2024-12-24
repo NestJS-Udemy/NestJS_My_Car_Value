c33163d7e5ed251448e3a8a53e53e870b9245c46
aixion1506 <aixion1506@gmail.com>
2024-12-24 14:00:01 +0900

    Feat: NotFoundException를 이용한 예외처리

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,6 +7,7 @@ import {
   Patch,
   Post,
   Query,
+  NotFoundException,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
@@ -22,8 +23,10 @@ export class UsersController {
   }
 
   @Get('/:id')
-  findUser(@Param('id') id: string) {
-    return this.userServcie.findOne(parseInt(id));
+  async findUser(@Param('id') id: string) {
+    const user = await this.userServcie.findOne(parseInt(id));
+    if (!user) throw new NotFoundException('user not found');
+    return user;
   }
 
   @Get()
