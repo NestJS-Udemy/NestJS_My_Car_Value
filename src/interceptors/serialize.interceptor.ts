24cf9fb01c2cebff0a2160dceee2147c2502167a
aixion1506 <aixion1506@gmail.com>
2024-12-30 15:17:16 +0900

    Refactor: class-transformer 업데이트로 인한 매서드 변경

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -6,7 +6,7 @@ import {
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { plainToClass } from 'class-transformer';
+import { plainToInstance } from 'class-transformer';
 
 export function Serialize(dto: any) {
   return UseInterceptors(new SerializeInterceptor(dto));
@@ -18,7 +18,7 @@ export class SerializeInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
     return handler.handle().pipe(
       map((data: any) => {
-        return plainToClass(this.dto, data, {
+        return plainToInstance(this.dto, data, {
           excludeExtraneousValues: true,
         });
       }),
