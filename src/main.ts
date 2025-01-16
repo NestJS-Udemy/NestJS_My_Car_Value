22ed866299679ec9bd5fd165a78700696e052955
aixion1506 <aixion1506@gmail.com>
2025-01-16 17:24:19 +0900

    Feat: 사용자 로그인 시키기

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(
     cookieSession({
-      key: ['asdf'],
+      keys: ['asdf'],
     }),
   );
   app.useGlobalPipes(
