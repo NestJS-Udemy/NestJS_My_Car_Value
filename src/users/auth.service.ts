b51bde231ac43171d4c77f481ef2269565f31892
aixion1506 <aixion1506@gmail.com>
2025-03-09 18:38:49 +0900

    Feat : 회원가입 및 로그인 기능 추가

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { promisify } from 'util';
 import { randomBytes, scrypt as _scrypt } from 'crypto';
@@ -22,5 +26,31 @@ export class AuthService {
     const [salt, storedHash] = storedPassword.split('.');
     const hash = (await scrypt(suppliedPassword, salt, 32)) as Buffer;
     return storedHash === hash.toString('hex');
+  }
+
+  async authSignup(email: string, password: string) {
+    // 이메일 중복 체크
+    try {
+      await this.usersService.find(email);
+      throw new BadRequestException('Email already in use');
+    } catch (error) {
+      // NotFoundException이면 정상적으로 회원가입 진행
+    }
+
+    // 비밀번호 해싱
+    const hashedPassword = await this.hashPassword(password);
+
+    // 유저 생성
+    return this.usersService.create(email, hashedPassword);
+  }
+
+  async authSignin(email: string, password: string) {
+    const [user] = await this.usersService.find(email);
+    if (!user) throw new NotFoundException('user not found');
+
+    const validPassword = await this.comparePasswords(user.password, password);
+    if (!validPassword) throw new BadRequestException('Invalid password');
+
+    return user;
   }
 }
