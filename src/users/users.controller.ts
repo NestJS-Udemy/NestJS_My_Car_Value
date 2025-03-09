59d58fea7ac4fd11748586f67d7cd41cf360f783
aixion1506 <aixion1506@gmail.com>
2025-03-09 19:36:07 +0900

    Feat : 세션으로 사용자 로그인 유지

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,21 +8,40 @@ import {
   Post,
   Query,
   NotFoundException,
+  Session,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 @Serialize(UserDto)
 export class UsersController {
-  constructor(private userServcie: UsersService) {}
+  constructor(
+    private userServcie: UsersService,
+    private authService: AuthService,
+  ) {}
+
+  @Get('/whoami')
+  whoAmI(@Session() session: any) {
+    return this.userServcie.findOne(session.userId);
+  }
 
   @Post('/signup')
-  createUser(@Body() body: CreateUserDto) {
-    this.userServcie.create(body.email, body.password);
+  async createUser(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.authSignup(body.email, body.password);
+    session.userId = user.id;
+    return user;
+  }
+
+  @Post('/signin')
+  async signin(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.authSignin(body.email, body.password);
+    session.userId = user.id;
+    return user;
   }
 
   @Get('/:id')
