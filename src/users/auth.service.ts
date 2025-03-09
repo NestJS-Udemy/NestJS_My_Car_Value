36fc2f61a888f835071dc7b1819dc32484baef2c
aixion1506 <aixion1506@gmail.com>
2025-03-09 18:38:13 +0900

    Feat : 비밀번호 해싱 및 검증 기능 추가

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,26 +1,26 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { promisify } from 'util';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+
+const scrypt = promisify(_scrypt);
 
 @Injectable()
 export class AuthService {
   constructor(private usersService: UsersService) {}
 
-  async signup(email: string, password: string) {
-    // See if email is in use
-    const users = await this.usersService.find(email);
-    if (users.length) throw new BadRequestException('email in use');
-
-    // Hash the users password
-    // Generate a salt
-
-    // Hash the salt and the password together
-
-    // Join the hased result and the salt together
-
-    // Create a new user and save it
-
-    // return the user
+  private async hashPassword(password: string): Promise<string> {
+    const salt = randomBytes(8).toString('hex');
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+    return `${salt}.${hash.toString('hex')}`;
   }
 
-  signin() {}
+  private async comparePasswords(
+    storedPassword: string,
+    suppliedPassword: string,
+  ): Promise<boolean> {
+    const [salt, storedHash] = storedPassword.split('.');
+    const hash = (await scrypt(suppliedPassword, salt, 32)) as Buffer;
+    return storedHash === hash.toString('hex');
+  }
 }
